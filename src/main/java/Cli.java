9928072ce7863d8b0c46cba1825b0654b986c617
lawbi <jevinnen@mail.ru>
2022-05-29 16:17:46 +0300

    add checkstyle

--- src/main/java/Cli.java
+++ src/main/java/Cli.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Cli {
     public static void greeting() {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("May I have your name?");
-       var name = sc.next();
-       System.out.println("Hello, " + name + "!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        var name = sc.next();
+        System.out.println("Hello, " + name + "!");
     }
 }
