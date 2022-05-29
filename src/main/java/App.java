0ff3e823e77aca6e5a2152dd47af20035746fd34
lawbi <jevinnen@mail.ru>
2022-05-29 16:57:59 +0300

    add game choosing draft

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter. \n1 - Greet \n0 - Exit");
+        var chose = scan.next();
+        System.out.println("Your choice: " + chose);
         System.out.println("Welcome to the Brain Games!");
         Cli.greeting();
     }
