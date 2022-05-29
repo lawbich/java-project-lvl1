ec70abd8b87545ebe2a89a86f8f3147a21607942
lawbi <jevinnen@mail.ru>
2022-05-29 15:43:58 +0300

    add structure

---
+++ Makefile
@@ -0,0 +1,11 @@
+run-dist:
+	./build/install/app/bin/app
+
+clean:
+	./gradlew clean
+
+build:
+	./gradlew clean build
+
+run:
+	./gradlew run
---
+++ src/main/java/Cli.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Cli {
+    public static void greeting() {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("May I have your name?");
+       var name = sc.next();
+       System.out.println("Hello, " + name + "!");
+    }
+}
